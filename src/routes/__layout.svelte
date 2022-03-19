7cbca67a1309ac31add05874eb5b9520e60be9f5
aeggydev <kucernak.a@gmail.com>
2022-03-19 11:16:16 +0100

    Make footer stick to bottom

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,20 +1,18 @@
 <script>
 	import Nav from '$lib/components/Nav.svelte';
 	import Header from '$lib/components/Header.svelte';
-    import Footer from "$lib/components/Footer.svelte";
+	import Footer from '$lib/components/Footer.svelte';
 </script>
 
-<div class="grid">
-	<aside>
-		<Nav />
-	</aside>
-	<div class="main-panel">
-		<Header />
-		<main>
-			<slot />
-		</main>
-        <Footer />
-	</div>
+<aside>
+	<Nav />
+</aside>
+<div class="main-panel">
+	<Header />
+	<main>
+		<slot />
+	</main>
+	<Footer />
 </div>
 
 <style>
@@ -22,28 +20,32 @@
 	:global(html, body) {
 		font-family: 'Outfit', Arial, sans-serif;
 		background: #fff8dd;
-        padding-left: 0.25em;
-        padding-top: 0;
-        margin: 0;
-    }
-    :global()::selection {
-        background: #69517D;
-        color: white;
-    }
-    :global()::-webkit-scrollbar {
-        background: transparent;
-        width: 0.75rem;
-    }
-    :global()::-webkit-scrollbar-thumb {
-        background: #939CA8;
-    }
-    aside {
-        left: 1.5%;
-        top: 2%;
-        max-width: 7%;
-        position: fixed;
-    }
-    .main-panel {
-        margin: 2em 20%;
-    }
+		padding-left: 0.25em;
+		padding-top: 0;
+		margin: 0;
+		min-height: 100vh;
+	}
+	:global()::selection {
+		background: #69517d;
+		color: white;
+	}
+	:global()::-webkit-scrollbar {
+		background: transparent;
+		width: 0.75rem;
+	}
+	:global()::-webkit-scrollbar-thumb {
+		background: #939ca8;
+	}
+	aside {
+		left: 1.5%;
+		top: 2%;
+		max-width: 7%;
+		position: fixed;
+	}
+	.main-panel {
+        min-height: 100vh;
+		display: grid;
+		grid-auto-rows: auto 1fr auto;
+		padding: 0 20%;
+	}
 </style>
