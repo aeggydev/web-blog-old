2bbfd6cbe6b83051281c3868a8bff708d90b0b1f
aeggydev <kucernak.a@gmail.com>
2022-03-19 11:22:52 +0100

    Make Nav use scss

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,9 +1,9 @@
 <div class="nav">
 	<div>My name is nellerdev</div>
 	<div class="links">
-        <a href="/">Articles</a>
+		<a href="/">Articles</a>
 		<a href="/about">About</a>
-        <a href="/projects">Projects</a>
+		<a href="/projects">Projects</a>
 		<a href="/contact">Contact</a>
 		<a href="https://github.com/aeggydev" target="_blank">Github</a>
 	</div>
@@ -13,7 +13,7 @@
 	</div>
 </div>
 
-<style>
+<style lang="scss">
 	.nav {
 		display: flex;
 		flex-direction: column;
@@ -24,14 +24,14 @@
 		flex-direction: column;
 		align-items: flex-end;
 		padding-right: 20%;
+		a {
+			color: #632626;
+			text-decoration: none;
+			&:hover {
+				text-decoration: underline;
+			}
+		}
 	}
-	.links a {
-		color: #632626;
-		text-decoration: none;
-	}
-    .links a:hover {
-        text-decoration: underline;
-    }
 	.icons {
 		display: flex;
 		align-items: center;
