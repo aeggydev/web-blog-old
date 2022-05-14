f4e1b8c7f73cfe1240ce8df93d4ad925b6225f85
aeggydev <kucernak.a@gmail.com>
2022-05-14 20:51:02 +0200

    Add under construction signs to sites being worked on

---
+++ src/lib/components/UnderConstruction.svelte
@@ -0,0 +1,13 @@
+<div>
+    <h3>This section is being worked on</h3>
+    <img src="/under_construction.svg" alt="Under construction">
+</div>
+
+<style>
+    div {
+        display: grid;
+        place-items: center;
+        padding: 1rem;
+        text-align: center;
+    }
+</style>
