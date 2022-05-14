f409b19cbb722f66d8039c476407a3435afcc37d
aeggydev <kucernak.a@gmail.com>
2022-05-14 17:10:14 +0200

    Remove special footer font

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -3,10 +3,7 @@
 </footer>
 
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
-
     footer {
-        font-family: 'Roboto', sans-serif;
         text-align: center;
         margin: 1.5rem 0;
         row-gap: 0.2rem;
